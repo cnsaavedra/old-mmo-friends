dbcf4246cfb28d8b83a0738301a2afd6662734dd
cnsaavedra <christian-saavedra@hotmail.com>
2019-03-16 22:43:19 -0400

    made the regex better for register

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,10 +1,14 @@
 const AutenthicationController = require('./controllers/AuthenticationController')
 
+// acts as the middleware function before we hit controller (whichever middleware we defined)
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 
 //simplifies and keeps everything isolated
 // Controllers used for declaring endpoints
 // Routes are for declaring routes that point to the controllers
 module.exports = (app) => {
     app.post('/register', 
+        AuthenticationControllerPolicy.register,
         AutenthicationController.register)
 }
