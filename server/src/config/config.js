b656992d868225a55c16394b39d709a56c9ddfc4
cnsaavedra <christian-saavedra@hotmail.com>
2019-03-16 22:07:37 -0400

    port was the problem, fixed by changing

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,11 +3,12 @@ module.exports = {
     port: process.env.PORT || 8081,
     db: {
         database: process.env.DB_NAME || 'accounts',
-        user: process.env.DB_USER || 'superUser',
+        user: process.env.DB_USER || 'useracc',
         password: process.env.DB_PASS || '123456',
         options:{
             dialect: process.env.DIALECT || 'postgres',
-            host: process.env.HOST || 'localhost'
+            host: process.env.HOST || 'localhost',
+            port: '5433'
         }
     }
 }
