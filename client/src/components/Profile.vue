88623957303616e2b302f2c4423094bb82644e97
cnsaavedra <christian-saavedra@hotmail.com>
2019-03-25 14:46:45 -0400

    trying to add game tied with username

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -8,10 +8,47 @@
                 <!-- unique identifier for gamename is ign -->
                 <div v-for="gamename in gamenames"
                     :key="gamename.ign">
-                    {{gamenames}}:
-                    {{gamename}}
+                    {{gamenames.ign}}:
+                    {{gamenames.gamename}}
                 </div>
             </panel>
+            <v-spacer></v-spacer>
+            <v-layout row justify-center>
+                <v-dialog v-model="adder" persistent max-width="290">
+                    <template v-slot:activator="{ on }">
+                        <v-btn
+                            outline
+                            large
+                            fab
+                            v-on="on"
+                            color="indigo">
+                        <v-icon>edit</v-icon>
+                        </v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-title class="headline">Add an IGN!</v-card-title>
+                            <v-flex xs6 offset-xs3>
+                                <v-text-field
+                                    name="ign"
+                                    v-model="gamenames.ign"
+                                    placeholder="IGN"
+                                ></v-text-field>
+                            </v-flex>
+                            <v-flex xs6 offset-xs3>
+                                <v-text-field
+                                    name="gamename"
+                                    v-model="gamenames.gamename"
+                                    placeholder="Game"
+                                ></v-text-field>
+                            </v-flex>
+                        <v-card-text>Please type in as accurate as possible your IGN along with the game played with it.</v-card-text>
+                        <v-card-actions>
+                            <v-btn color="green darken-1" flat @click="adder = false">Cancel</v-btn>
+                            <v-btn color="green darken-1" flat @click="add">Add</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-layout>
         </v-flex>
     </v-layout>
 </template>
@@ -24,7 +61,25 @@ export default {
     data () {
         return {
             username: '',
-            gamenames: ''
+            gamenames: {
+                ign: '',
+                gamename: ''
+            },
+            adder: false,
+            adding: false
+        }
+    },
+    methods: {
+        async add () {
+            this.adder = false
+            console.log('Added game.')
+            try {
+                if (this.adding === true) {
+                    console.log(this.adding)
+                }
+            } catch (err) {
+                console.log(err)
+            }
         }
     },
     async mounted () {
